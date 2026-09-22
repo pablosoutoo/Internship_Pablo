9d240492b0e86e22e9ee4ee831e45d6e93de1814
Pablo Souto García <pablosoutogarcia@gmail.com>
2026-09-22 13:49:16 +0200

    Update of the script and the volcano plot because the points were too big.

--- Module1 (Exploratory Analysis)/s1_script.R
+++ Module1 (Exploratory Analysis)/s1_script.R
@@ -1,3 +1,19 @@
-R.version.string
-#> [1] "R version 4.5.1 (2025-06-13)"
-slect
+set.seed()
+#Download the necessary libraries
+library(dplyr)
+library(Seurat)
+library(qs2)
+library(ggplot2)
+
+#Read Data
+data<-qs_read("Inputs/External/stripped_s1_harmony_tumor_cells.qs2")
+data
+
+#Standard preprocessing workflow
+
+##Calculate the mitochondrial QC metrics 
+data[["percent.mt"]] <-PercentageFeatureSet(data, pattern = "^MT-")
+
+##Visualize in plots
+volcano_plot<-VlnPlot(data, features =c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol=3)
+ggsave(filename="Module1 (Exploratory Analysis)/results/s1_mitocond_pres.png", plot=volcano_plot,width = 8, height = 6, dpi = 300)
